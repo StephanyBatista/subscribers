cbe9ae8d60589ff9238cc3799c398748d531429e
Stephany Batista <stephany.batista@nibo.com.br>
2022-07-25 21:18:41 -0400

    creating the api project

---
+++ api/README.md
@@ -0,0 +1,10 @@
+# Install
+
+In order to install all dependencies of project, execute the cmd below.
+
+```go get```
+
+# Exec project
+In order to execute do the cmd below and the api will be on :6004 port
+
+```air```
---
+++ api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/healthcheck", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "aa"})
+	})
+	r.Run(":6004")
+}
