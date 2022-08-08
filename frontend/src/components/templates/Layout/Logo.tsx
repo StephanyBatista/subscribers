105b10292cd46d48bc27b336c03064fba9c134ff
felipe almeida batista <felipeab10@hotmail.com>
2022-08-08 19:19:26 -0400

    Created page for list client and add client

--- frontend/src/components/templates/Layout/Logo.tsx
+++ frontend/src/components/templates/Layout/Logo.tsx
@@ -1,10 +1,17 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Link } from "@chakra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
 
 export function Logo() {
     return (
-        <Box >
-            <Heading fontSize="xl" as="span" >Subs</Heading>
-            <Heading fontSize="xl" color="blue.900" as="span" >criber</Heading>
-        </Box>
+        <Link
+            _hover={{ textDecoration: 'none' }}
+            as={ReactLink}
+            to="/dashboard"
+        >
+            <Box >
+                <Heading fontSize="xl" as="span" >Subs</Heading>
+                <Heading fontSize="xl" color="blue.900" as="span" >criber</Heading>
+            </Box>
+        </Link>
     )
 }
--- frontend/src/services/routes/Router.tsx
+++ frontend/src/services/routes/Router.tsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { Campaigns } from "../../pages/Campaigns";
 import { Create } from "../../pages/Campaigns/Create";
 import { Edit } from "../../pages/Campaigns/Edit";
+import { Clients } from "../../pages/Clients";
+import { CreateClient } from "../../pages/Clients/Create";
+import { EdtiClient } from "../../pages/Clients/Edit";
 import { Dashboard } from "../../pages/Dashboard";
 import { Register } from "../../pages/Register";
 import { SingIn } from "../../pages/SingIn";
@@ -21,6 +24,11 @@ export function AppRoute() {
                 <Route path="create" element={<Create />} />
                 <Route path="edit/:campaignId" element={<Edit />} />
                 <Route index element={<Campaigns />} />
+            </Route>
+            <Route path="clients" >
+                <Route path="create" element={<CreateClient />} />
+                <Route path="edit/:clientId" element={<EdtiClient />} />
+                <Route index element={<Clients />} />
             </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
